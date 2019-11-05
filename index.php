70561b5938fdcf8d9af71aa73dab6bc7dab9e337
freddiemixell <fmixell@gmail.com>
2019-11-05 18:12:02 -0500

    better routing

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
-
 require_once get_template_directory() . '/vendor/simple-html-dom/simple_html_dom.php';
 
-$define_path = get_template_directory_uri() . "/app/public{$request}index.html";
+$request = (string) $_SERVER['REQUEST_URI'];
+$length = strlen( $request );
+
+switch ( true ) {
+    case $length === 1:
+        $define_path = get_template_directory_uri() . "/app/public/index.html";
+        break;
+    case $length > 1:
+        $normalize_request = stripslashes( $request );
+        $define_path = get_template_directory_uri() . "/app/public/{$normalize_request}/index.html";
+        break;
+    default:
+        $define_path = get_template_directory_uri() . "/app/public/index.html";
+        break;
+}
 
 $route = file_get_html( $define_path );
 
